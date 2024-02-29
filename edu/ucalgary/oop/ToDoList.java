beb1b164b5c1947e7afe0906329a9355ab21f56a
Umar5426 <114004528+Umar5426@users.noreply.github.com>
2024-02-29 15:05:28 -0700

    edittask() updated

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -15,31 +15,41 @@ public class ToDoList implements IToDoList {
     }
 
     @Override
-    public void completeTask(Task task) {
+    public void completeTask(Task task){
         task.setCompleted(true);
         System.out.println("Task completed: " + task.getTitle());
     }
 
     @Override
-    public void deleteTask(Task task) {
+    public void deleteTask(Task task){
         tasks.remove(task);
         System.out.println("Task deleted: " + task.getTitle());
     }
 
     @Override
-    public void editTask(Task task) {
-        //ADD LOGIC
-        System.out.println("Task edited: " + task.getTitle());
+    public void editTask(String taskId, String newTitle, boolean isCompleted) {
+        //finding the task using the id
+        for (Task task : tasks) {
+            if (task.getId().equals(taskId)) { //Changing/Editing the task itself
+                task.setTitle(newTitle);
+                task.setCompleted(isCompleted);
+                System.out.println("Task edited: " + task.getTitle());
+                return;
+            }
+        }
+        // If task with given Id is not found
+        System.out.println("Task with ID " + taskId + " not found");
     }
 
+
     @Override
-    public void undo() {
+    public void undo(){
         //ADD LOGIC
         System.out.println("Undo operation performed");
     }
 
     @Override
-    public void listTasks() {
+    public void listTasks(){
         System.out.println("Tasks:");
         for (Task task : tasks) {
             System.out.println("- " + task.getTitle() + " (Completed: " + task.isCompleted() + ")");
