00a04b1253e5d60c7ddee3d0600e30f6d45d5776
Deven <32754187+fbyrgy@users.noreply.github.com>
2024-02-29 15:22:13 -0700

    edited file

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class ToDoList implements IToDoList {
     private List<Task> tasks;
+    private Stack<List<Task>> history;
 
     public ToDoList() {
         tasks = new ArrayList<>();
@@ -11,18 +12,21 @@ public class ToDoList implements IToDoList {
     @Override
     public void addTask(Task task){
         tasks.add(task);
+        saveHistory();
         System.out.println("Task added: " + task.getTitle());
     }
 
     @Override
     public void completeTask(Task task){
         task.setCompleted(true);
+        saveHistory();
         System.out.println("Task completed: " + task.getTitle());
     }
 
     @Override
     public void deleteTask(Task task){
         tasks.remove(task);
+        saveHistory();
         System.out.println("Task deleted: " + task.getTitle());
     }
 
@@ -38,14 +42,19 @@ public class ToDoList implements IToDoList {
             }
         }
         // If task with given Id is not found
+        saveHistory();
         System.out.println("Task with ID " + taskId + " not found");
     }
 
 
     @Override
-    public void undo(){
-        //ADD LOGIC
-        System.out.println("Undo operation performed");
+    public void undo() {
+        if (!history.isEmpty()) {
+            tasks = new ArrayList<>(history.pop());
+            System.out.println("Undo operation performed");
+        } else {
+            System.out.println("Nothing to undo");
+        }
     }
 
     @Override
@@ -54,5 +63,10 @@ public class ToDoList implements IToDoList {
         for (Task task : tasks) {
             System.out.println("- " + task.getTitle() + " (Completed: " + task.isCompleted() + ")");
         }
+    }
+
+    private void saveHistory() {
+        List<Task> taskCopy = new ArrayList<>(tasks);
+        history.push(taskCopy);
     }
 }
